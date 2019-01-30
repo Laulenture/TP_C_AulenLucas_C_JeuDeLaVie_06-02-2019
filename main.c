0a0908dabd5965f0323c45ba7b1a7ebc3eac6bba
LAULENOS-PORTAB\Hikaru <l.aulen@ludus-academie.com>
2019-01-30 12:46:05 +0100

    no message

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+    i : int             :nVar
+    f : float           :flVar
+    d : double          :dblVar
+    c : char            :cVar
+    l : long int        :lnVar
+    ll: long long int   :llnVar
+    L : double long     :ldblVar
+    h : short int       :srVar
+    ui: unsigned int    :unVar
+    uf: unsigned float  :unfVar
+    s : *STRING*
+    * : *POINTEUR*      :pVar
+*/
+
+/**
+  * BUT : 
+  * ENTREE : 
+  * SORTIE : 
+  */
+int main()
+{
+    
+    return 0;
+}
